c43a226cd69d1821ce422e8d684b016cfe0c5088
Arpitsinh Vaghela <arpitvaghela9210@gmail.com>
2020-01-29 14:52:53 +0530

    added nav-links

--- nav.js
+++ nav.js
@@ -17,3 +17,19 @@ document.getElementById('clear').addEventListener('click',()=>{
 document.getElementById('menu-icon').addEventListener('click',()=>{
     document.getElementById('nav-list').classList.remove('nav-hide');
 })
+let ll = document.getElementsByClassName('link')
+
+for (let link of ll) {
+    link.addEventListener('click',()=>{
+    document.getElementById('nav-list').classList.add('nav-hide');
+    })
+}
+$('a[href*=\\#]:not([href=\\#])').on('click',()=>{
+    let target = $(this.hash);
+    if (target.length){
+        $('html,body').animate({
+            scrollTop:target.offset().top
+        },1000);
+        return false;
+    }
+})
